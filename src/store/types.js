58bdf5cfee59d5fceb999b9f441d3570385a78f3
yiqunbaichi <xuwei0498@163.com>
2017-12-19 14:08:50 +0800

    Revert "init"
    This reverts commit e0ac1def5ae25adf88d2e1b35fffbeb8c4174360.

--- src/store/types.js
+++
@@ -1,4 +0,0 @@
-export const LOGIN = 'login'
-export const LOGOUT = 'logout'
-export const TITLE = 'title'
-export const TERMINAL = 'terminal'
--- ws/config/wsBartenderConfig.js
+++
@@ -1,8 +0,0 @@
-// bartender
-var ws_b_config = {
-  tb_bartender_list: 'tb_bartender_list:',
-  tb_bartender_detail: 'tb_bartender_detail:',
-  tb_bartender_poslist: 'tb_bartender_poslist'
-
-}
-module.exports = ws_b_config
