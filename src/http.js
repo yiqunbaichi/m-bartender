88babe18e0c5ee32a24e606cdef722dbe1f0aa97
yiqunbaichi <xuwei0498@163.com>
2017-12-22 11:06:47 +0800

    change

--- src/http.js
+++ src/http.js
@@ -6,7 +6,8 @@ import router from './router'
 // axios 配置
 axios.defaults.timeout = 5000
 // axios.defaults.baseURL = 'http://api.riowine.com:8070'
-axios.defaults.baseURL = 'http://192.168.0.102:8781/'
+axios.defaults.baseURL = 'http://localhost:8781/'
+
 // http request 拦截器
 axios.interceptors.request.use(
   config => {
