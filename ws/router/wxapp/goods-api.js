02f061089e05674600755e464fb3cd3334c333dd
yiqunbaichi <xuwei0498@163.com>
2018-04-26 18:35:26 +0800

    20180426新增 根据产品id获取产品详细信息

--- ws/router/wxapp/goods-api.js
+++ ws/router/wxapp/goods-api.js
@@ -16,4 +16,23 @@ router.get('/getGoodsList', function (req, res, next) {
       res.send(rm.getFailRM('', '', ''))
     })
 })
+
+router.get('/getProductById', function (req, res, next) {
+    // let terminalId = req.query.terminalId
+    let url = '/RPCService/webservice/goodsRestApi/getProductById'
+    http.get(url,{params: {
+        sourceType: req.query.sourceType,
+        productId:req.query.productId
+    }
+    })
+        .then(response => {
+            if (response.data.resultCode === 200) {
+                res.send(rm.getSuccessRM('', JSON.parse(response.data.resultValue)))
+            } else {
+                res.send(rm.getFailRM('', JSON.parse(response.data.resultDesc), JSON.parse(response.data.resultValue)))
+            }
+        }).catch(error => {
+        res.send(rm.getFailRM('', '', ''))
+    })
+})
 module.exports = router
