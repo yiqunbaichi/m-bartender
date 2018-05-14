d559f142f11fcb17271f70cb7c587e40553e1d02
yiqunbaichi <xuwei0498@163.com>
2018-05-14 18:23:23 +0800

    修改接口

--- ws/config/wsBartenderConfig.js
+++ ws/config/wsBartenderConfig.js
@@ -3,9 +3,10 @@ var ws_b_config = {
     tb_terminal_detail: 'tb_terminal_detail:',
     tb_terminal_poslist: 'tb_terminal_poslist',
 
-  wxapp_tb_js_token: 'wxapp_tb_js_token:'
-
-
+    wxapp_tb_js_token: 'wxapp_tb_js_token:',
+    access_token_wx58d61b73568eefd3: 'access_token_wx58d61b73568eefd3',
+    appid: 'wx58d61b73568eefd3',
+    secret: '68d2a7e8a3ff3e24654147f62c06d825',
 
 }
 module.exports = ws_b_config
