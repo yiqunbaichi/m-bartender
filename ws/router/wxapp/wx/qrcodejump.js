c1ed46d9771a7eff7b1b5e4482bb30c666afc582
yiqunbaichi <xuwei0498@163.com>
2018-05-29 13:51:08 +0800

    fix bug

--- ws/router/wxapp/wx/qrcodejump.js
+++ ws/router/wxapp/wx/qrcodejump.js
@@ -8,7 +8,6 @@ var router = express.Router()
 // })
 
 router.get('/ZJG7T8OAvE.txt', function (req, res, next) {
-    let terminalId = req.query.terminalId
     res.send("5476af810ce1276192d7eea83b4c7e21")
 
 })
